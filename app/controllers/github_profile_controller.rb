f9c0e3d9a10fd62e9ee589f7024e778e37bebe93
John M Buscemi <jmbuscemi@gmail.com>
2015-06-02 15:00:11 -0400

    Add index page with form

---
+++ app/controllers/github_profile_controller.rb
@@ -0,0 +1,7 @@
+class GithubProfileController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  root 'github_profile#index'
+
+  get 'github_profile/index'
+
+  post 'github_profile/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
